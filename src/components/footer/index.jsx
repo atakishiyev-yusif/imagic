a2a7e8e94195642386feb22830c2485728666707
Yusif Atakishiyev <yusifatakishiyev@proton.me>
2023-09-17 01:30:30 +0400

    initial commit ✨

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,9 +5,9 @@ import {FaSquareXTwitter, FaXTwitter} from "react-icons/fa6";
 //
 export const Footer = () => {
     return <footer className= 'flex items-center gap-6'>
-        <a href=""><AiOutlineGithub className= 'hover:text-blue-700 w-[25px] h-[25px]'/></a>
-        <a href=""><AiOutlineInstagram className= 'hover:text-blue-700 w-[25px] h-[25px]'/></a>
-        <a href=""><AiFillYoutube className= 'hover:text-blue-700 w-[25px] h-[25px]' /></a>
-        <a href=""><FaXTwitter className= 'hover:text-blue-700 w-[25px] h-[25px]' /></a>
+        <a href="https://github.com/atakishiyev-yusif" target="_blank" ><AiOutlineGithub className= 'hover:text-blue-700 w-[25px] h-[25px]'/></a>
+        <a href="https://www.instagram.com/yusifatakishiyev/" target="_blank" ><AiOutlineInstagram className= 'hover:text-blue-700 w-[25px] h-[25px]'/></a>
+        <a href="https://youtube.com/@yusif_atakishiyev?si=U_9ir6CaeNpQNGKH" target="_blank" ><AiFillYoutube className= 'hover:text-blue-700 w-[25px] h-[25px]' /></a>
+        <a href="https://twitter.com/ysfatakishiyev" target="_blank" ><FaXTwitter className= 'hover:text-blue-700 w-[25px] h-[25px]' /></a>
     </footer>
 }
