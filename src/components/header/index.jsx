7888152f61dbe2ca570339ad7d18e91a82342bd8
Yusif Atakishiyev <yusifatakishiyev@proton.me>
2023-09-17 00:44:56 +0400

    add link logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,9 @@
 import {Logo} from '../../assets/logo.jsx'
+import {Link} from "react-router-dom";
 export const Header = () => {
     return <header className= 'p-6 flex items-center justify-between'>
-        <div>
+        <Link to = '/'>
            <span className= 'flex items-center gap-2'> <Logo />  <span className= 'text-xl font-semibold'>imagic</span></span>
-        </div>
+        </Link>
     </header>
 }
